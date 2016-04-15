140aaee0d73998b0c22ee716a9f6ea2ccf1f1e3b
tetradog <tetradog@gmail.com>
2016-04-15 16:32:57 +0200

    inici del projecte

---
+++ BancDelTempsOnline/Window1.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Pingu
+ * Fecha: 15/04/2016
+ * Hora: 16:31
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace BancDelTempsOnline
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
